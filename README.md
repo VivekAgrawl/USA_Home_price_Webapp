6f9dc97699d2e753a95c2f4844ac8f1a9d59825a
Vivek Agrawal <vivekanalytics2021@gmail.com>
2023-11-26 22:39:52 +0530

    Write Insights

---
+++ README.md
@@ -0,0 +1,15 @@
+# Factors Impacting Home Price Trends
+
+### Project Overview
+Investigate the factors influencing US home prices over the last two decades through a data-driven analysis. Utilizing datasets from [fred.stlouisfed.org](https://fred.stlouisfed.org/), key factors are considered to understand their impact on the S&P Case Schiller Home Price Index.
+            
+### Key Factors
+Population, Producer Price Index (Cement & Concrete, Bricks), Unemployment Rate, Personal Income, GDP, Construction Employment, Labor Force Participation Rate, Monthly Supply of New Houses, Federal Funds Effective Rate, and Median Consumer Price Index.
+
+### Data Science Model
+Using advanced analytics, our model uncovers patterns and relationships, shedding light on how these factors impact home prices.
+            
+### Why It Matters
+For homeowners, buyers, policymakers, and investors, this analysis offers valuable insights into the forces driving real estate dynamics. Explore our web app for interactive visualizations and detailed analyses. Uncover the story behind the numbers, empowering informed decisions in the ever-evolving US housing landscape.
+             
+Explore the [web application](https://home-prices.streamlit.app/) to understand how our data-driven analysis unveils insights into the factors shaping the US housing market.
--- app.py
+++ app.py
@@ -6,30 +6,31 @@ import joblib
 
 st.set_page_config(page_title = "Home Price Index", page_icon = "🏘")
 
-st.title("Factors Impacting Home Price Trends")
+st.title("🏡 Factors Impacting Home Price Trends 📈")
+st.write("")
 st.divider()
 data = pd.read_csv("data_cleaned/house_price_index.csv", index_col='date')
-tab1, tab2, tab3= st.tabs(['Home Price Index', 'Factors Analysis', 'About the Project'])
+tab1, tab2, tab3= st.tabs(['🏡 Home Price Index', '🔍 Factor Analysis', '📑 About the Project'])
 
 with tab1:
-    st.info("Population, Producer Price Index (Cement), Producer Price Index (Concrete Bricks), Unemployment Rate, Personal Income and Gross Domestic Product are the factors impacting Home Prices.", icon = "ℹ️")
-    st.write("Utilize the slider to adjust different factors and observe their influence on the Home Price Index.")
+    st.write("Exploring two decades of US home price trends, our data-driven analysis unveils key influencers. Factors such as Population, Producer Price Index (Cement), Producer Price Index (Concrete Bricks), Unemployment Rate, Personal Income, and Gross Domestic Product emerge as significant drivers of home prices. ")
+    st.write("Use the slider to manipulate these variables and observe their direct impact on the Home Price Index. ")
     col1, col2 = st.columns([2, 2], gap = "large")
     
     with col1:
         st.divider()
         st.subheader("User Input")
-        population = st.slider("Population", 290000, 340000, 317270)
-        PPI_cement = st.slider("Producer Price index (Cement)", 149.40, 332.00, 208.00)
-        PPI_concrete = st.slider("Producer Price Index (Concrete Brick)", 154.00, 337.00, 201.00)
-        unemployment = st.slider("Unemployment Rate", 3.40, 14.70, 5.30)
-        income = st.slider("Personal Income", 9500.00, 25000.00, 14000.00)
-        GDP = st.slider("Gross Domestic Product", 11500.00, 28000.00, 17000.00)
-        emp_cons = st.slider("All Employees, Construction", 5420.00, 8000.00, 7002.00)
-        fed_funds = st.slider("Federal Funds Effective Rate", 0.00, 6.00, 0.40)
-        houses_supply = st.slider("Monthly Supply of New Houses", 3.00, 12.50, 5.65)
-        lab_force = st.slider("Labor Force Participation Rate", 60.00, 67.00, 63.30)
-        median_CPI = st.slider("Median Consumer Price Index", -0.30, 8.50, 2.45)
+        population = st.slider("**Population (Thousands)**", 290000, 340000, 317270)
+        PPI_cement = st.slider("**Producer Price index (Cement)**", 149.40, 332.00, 208.00, help="The Producer Price Index (PPI) measures the average change over time in the selling prices received by domestic producers for their output.")
+        PPI_concrete = st.slider("**Producer Price Index (Concrete Brick)**", 154.00, 337.00, 201.00, help="The Producer Price Index (PPI) measures the average change over time in the selling prices received by domestic producers for their output.")
+        unemployment = st.slider("**Unemployment Rate (%)**", 3.40, 14.70, 5.30, help = "The unemployment rate represents the number of unemployed as a percentage of the labor force.")
+        income = st.slider("**Personal Income (Billions of Dollars)**", 9500.00, 25000.00, 14000.00, help = "Personal income is the income that persons receive in return for their provision of labor, land, and capital used in current production and the net current transfer payments that they receive from business and from government.")
+        GDP = st.slider("**Gross Domestic Product (Billions of Dollars)**", 11500.00, 28000.00, 17000.00, help = "Gross domestic product (GDP) is the market value of the goods and services produced by labor and property located in the United States.")
+        emp_cons = st.slider("**All Employees, Construction (Thousands of Persons)**", 5420.00, 8000.00, 7002.00, help = "Construction employees in the construction sector include: Working supervisors, qualified craft workers, mechanics, apprentices, helpers, laborers, and so forth, engaged in new work, alterations, demolition, repair, maintenance, and the like, whether working at the site of construction or in shops or yards at jobs (such as precutting and preassembling) ordinarily performed by members of the construction trades.")
+        fed_funds = st.slider("**Federal Funds Effective Rate (%)**", 0.00, 6.00, 0.40, help = "The rate that the borrowing institution pays to the lending institution is determined between the two banks; the weighted average rate for all of these types of negotiations is called the effective federal funds rate.")
+        houses_supply = st.slider("**Monthly Supply of New Houses**", 3.00, 12.50, 5.65, help = "The months' supply is the ratio of new houses for sale to new houses sold.")
+        lab_force = st.slider("**Labor Force Participation Rate (%)**", 60.00, 67.00, 63.30, help = 'The Labor Force Participation Rate is defined by the Current Population Survey (CPS) as “the number of people in the labor force as a percentage of the civilian noninstitutional population […] the participation rate is the percentage of the population that is either working or actively looking for work."')
+        median_CPI = st.slider("**Median Consumer Price Index**", -0.30, 8.50, 2.45, help = "Median Consumer Price Index (CPI) is a measure of core inflation calculated the Federal Reserve Bank of Cleveland and the Ohio State University.")
         df = {'emp_cons' : emp_cons, 'fed_funds': fed_funds, 'GDP': GDP, 'houses_supply': houses_supply, "income": income, "lab_force": lab_force, "median_CPI": median_CPI, "population": population, "PPI_cement": PPI_cement, "PPI_concrete": PPI_concrete, "unemployment": unemployment}
         df = pd.DataFrame(df, index = [0])
         
@@ -39,13 +40,13 @@ with tab1:
         st.subheader("Home Price Index")
         model = joblib.load("Model/model.pkl")
         predictions = model.predict(df)
-        st.success(f"{predictions[0]:.2f}")
+        st.success(f"**Prediction:** {predictions[0]:.2f}")
 
         # ------HPI Histogram-------------
         base = alt.Chart(data)
         bar = base.mark_bar(color = "steelblue").encode(
-            alt.X('HPI:Q').bin().axis(None),
-            y='count()'
+            alt.X('HPI:Q', title = "Home Price Index").bin(),
+            alt.Y('count()', title = 'Frequency')
         )
         rule = base.mark_rule(color='#ff4b4b').encode(
             x=alt.datum(predictions[0]),
@@ -54,12 +55,12 @@ with tab1:
         st.altair_chart(bar+rule, use_container_width=True)
 
 with tab2:
-    st.write("Utilizing the Random Forest Regression Algorithm with the Gini criterion, we conducted a comprehensive feature importance analysis, achieving an impressive R2 Score of 99.48%. The R2 Score indicates the model's ability to explain the variance in the Home Price Index using the selected features. The feature importance values showcase the significance of each factor in influencing the predictive accuracy of the model.")
+    st.write("Implemented the Random Forest Regression Algorithm for a comprehensive feature importance analysis, achieving a notable R2 Score of 99.48%. This score indicates the model's effectiveness in explaining Home Price Index variance with selected features. Explore feature importance values to understand each factor's impact on enhancing predictive accuracy.")
     st.write(" ")
     df_feature = pd.read_csv("Model/feature_importance.csv")
     corr_chart = alt.Chart(df_feature, title = 'Feature Importance in Predicting Home Prices').mark_bar().encode(
             alt.Y("Feature", title = " ").sort("-x"),
-            alt.X("Importance_Value:Q", title = "Importance Value"),
+            alt.X("Importance_Value:Q", title = "Feature Importance Score"),
             color= alt.value("steelblue")
         ).properties(height = 400)
     st.altair_chart(corr_chart, use_container_width=True)
@@ -87,13 +88,10 @@ with tab2:
 with tab3:
     st.write("""
             ### Project Overview
-            Explore the factors impacting US home prices over the last two decades through our data-driven analysis. Leveraging datasets from fred.stlouisfed.org, we've considered key factors such as population, unemployment rate, GDP, and more to understand their influence on the S&P Case Schiller Home Price Index.
+            Investigate the factors influencing US home prices over the last two decades through a data-driven analysis. Utilizing datasets from [fred.stlouisfed.org](https://fred.stlouisfed.org/), key factors are considered to understand their impact on the S&P Case Schiller Home Price Index.
             
              ### Key Factors
             Population, Producer Price Index (Cement & Concrete, Bricks), Unemployment Rate, Personal Income, GDP, Construction Employment, Labor Force Participation Rate, Monthly Supply of New Houses, Federal Funds Effective Rate, and Median Consumer Price Index.
-            
-             ### Data Collection
-            Our datasets span 20 years, sourced from fred.stlouisfed.org, ensuring a comprehensive view of long-term trends.
             
              ### Data Science Model
             Using advanced analytics, our model uncovers patterns and relationships, shedding light on how these factors impact home prices.
@@ -101,7 +99,7 @@ with tab3:
              ### Why It Matters
             For homeowners, buyers, policymakers, and investors, this analysis offers valuable insights into the forces driving real estate dynamics. Explore our web app for interactive visualizations and detailed analyses. Uncover the story behind the numbers, empowering informed decisions in the ever-evolving US housing landscape.
              
-             Explore the code on [GitHub](https://github.com/VivekAgrawl/USA_Home_price_Webapp.git) to understand how our data-driven analysis unveils insights into the factors shaping the US housing market.
+            Explore the code implementation on [GitHub](https://github.com/VivekAgrawl/USA_Home_price_Webapp.git) to understand how our data-driven analysis unveils insights into the factors shaping the US housing market.
              """)
 
 st.write("---")
